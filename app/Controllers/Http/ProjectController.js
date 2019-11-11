11e2e8778f0d9d6b203c82c92ce76af69d64a696
Luiz <lg.luizh@gmail.com>
2019-11-11 17:34:16 -0300

    Criado o cadastro de projetos

---
+++ app/Controllers/Http/ProjectController.js
@@ -0,0 +1,95 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Project = use('App/Models/Project')
+
+/**
+ * Resourceful controller for interacting with projects
+ */
+class ProjectController {
+  /**
+   * Show a list of all projects.
+   * GET projects
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const projects = await Project.all()
+    return projects
+  }
+
+  /**
+   * Create/save a new project.
+   * POST projects
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response, auth }) {
+    const data = request.only('name')
+    const project = await Project.create({
+      ...data,
+      user_id: auth.user.id
+    })
+
+    await project.load('user')
+
+    return project
+  }
+
+  /**
+   * Display a single project.
+   * GET projects/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing project.
+   * GET projects/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update project details.
+   * PUT or PATCH projects/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a project with id.
+   * DELETE projects/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = ProjectController
---
+++ database/migrations/1573500576903_project_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProjectSchema extends Schema {
+  up () {
+    this.create('projects', (table) => {
+      table.increments()
+      table.integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+      table.string('name')
+      table.boolean('done')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('projects')
+  }
+}
+
+module.exports = ProjectSchema
